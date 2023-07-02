46bcee19e35b47551a594cad7a9abd6f59153cbc
Роман <muzzlestore@gmail.com>
2023-07-02 14:04:08 +0300

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://muzzle:<password>@phonebook.xrsqkmm.mongodb.net/";
+
+mongoose.connect(DB_HOST);
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
