cb4c716158389c5056d8ff90245df6ab1d34a989
Роман <muzzlestore@gmail.com>
2023-07-18 02:31:52 +0300

    add Jimp manipulation

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const { User } = require("../../models");
 
@@ -11,6 +12,15 @@ const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
   const newFilename = `${_id}_${originalname}`;
+
+  await Jimp.read(tempUpload)
+    .then((avatar) => {
+      avatar.resize(250, 250).quality(60).write(tempUpload);
+    })
+    .catch((err) => {
+      throw HttpError(403, err.message);
+    });
+
   const resultUpload = path.join(avatarsDir, newFilename);
   await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", newFilename);
